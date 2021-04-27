7c9b5d096b4d359a82af1d625777e2c70b536a98
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-27 16:27:28 +0200

    typo

--- minmt_serie_train.sh
+++ minmt_serie_train.sh
@@ -21,6 +21,6 @@ python3 $setup -dnet $dnet -src_voc $dir/enfr.BPE.32k.voc -tgt_voc $dir/enfr.BPE
 echo setup ok
 echo debut du training
 
-CUDA_VISIBLE_DEVICES=0 python3 $train -dnet $dnet -src_train data_corpus/trn_src   -pre_train data_corpus/trn_pre  -tgt_train data_corpus/trn_tgt  -src_valid data_corpus/val_src -sim_valid data_corpus/val_sim  -pre_valid data_corpus/val_pre  -tgt_valid data_corpus/val_tgt -max_steps 750000 -loss KLDiv -cuda -log_file $dnet/log &
+CUDA_VISIBLE_DEVICES=0 python3 $train -dnet $dnet -src_train data_corpus/trn_src   -pre_train data_corpus/trn_pre  -tgt_train data_corpus/trn_tgt  -src_valid data_corpus/val_src   -pre_valid data_corpus/val_pre  -tgt_valid data_corpus/val_tgt -max_steps 750000 -loss KLDiv -cuda -log_file $dnet/log &
 
 echo fin du training
